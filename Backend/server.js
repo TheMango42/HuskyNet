3b56095caae3cb5f629ef5de5c781da701e19865
AnakynSaylor <ajsaylor@mtu.edu>
2025-03-12 12:44:33 -0400

    Made a json file to run server with "npm run start"

--- Backend/server.js
+++ Backend/server.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const port = 3306;
 
     const server = http.createServer((req, res) => {
       res.statusCode = 200;
@@ -6,7 +7,6 @@ const http = require('http');
       res.end('Hello, World!\n');
     });
 
-    const port = 3306;
 
     server.listen(port, () => {
       console.log(`Server running at http://localhost:${port}/`);
