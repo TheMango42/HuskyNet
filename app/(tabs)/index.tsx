f21c0abab9071855638df15249f96693b41be69a
Drew Broekhuis <dcbroekh@mtu.edu>
2025-03-21 14:23:22 -0400

    Got the search bar to work. It is only on the home page right now.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,27 +1,31 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { Image, StyleSheet, Platform, TextInput } from 'react-native';
 
+import React from 'react';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedView } from '@/components/ThemedView';
 import { FeedComponent } from '@/components/FeedComponent';
 
 export default function HomeScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/index.jpg')}
-          style={styles.headerImage}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <FeedComponent/>  
-        <FeedComponent/>  
-        <FeedComponent/>
-        <FeedComponent/>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
+  <ParallaxScrollView
+     headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+    headerImage = {
+      <Image
+        source={require('@/assets/images/index.jpg')}
+        style={styles.headerImage}
+      />
+    }>
+    <ThemedView style={styles.titleContainer}>
+    <ThemedView style={styles.textInputLight}>
+      <TextInput placeholder="Search"></TextInput>
+    </ThemedView>
+      <FeedComponent/>  
+      <FeedComponent/>  
+      <FeedComponent/>
+      <FeedComponent/>
+    </ThemedView>
+  </ParallaxScrollView>
+);
 }
 
 const styles = StyleSheet.create({
@@ -39,4 +43,24 @@ const styles = StyleSheet.create({
     resizeMode: "cover", // Makes the image fit while covering
     position: 'absolute',
   },
+  textInputLight: {
+    width: 200,
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 3,
+    borderRadius: 15,
+    paddingHorizontal: 10,
+    margin: 10,
+    color: 'black',
+},
+textInputDark: {
+    width: 200,
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 3,
+    borderRadius: 15,
+    paddingHorizontal: 10,
+    margin: 10,
+    color: 'white',
+}
 });
