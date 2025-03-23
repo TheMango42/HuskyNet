fd7fce59bbb2c28fd54fed24fef5dd76e66ec0dc
Josh Arango <arangojg05@gmail.com>
2025-03-23 18:33:25 -0400

    Update PostComponent.tsx
    Fixed oopsie

--- components/PostComponent.tsx
+++ components/PostComponent.tsx
@@ -3,6 +3,7 @@ import {View, Text, StyleSheet, Image, useColorScheme} from 'react-native';
 import React from 'react';
 import { AntDesign } from '@expo/vector-icons';
 import { Pressable } from 'react-native';
+import { ThemedText } from './ThemedText';
 
 type PostID = {
     id: string,
@@ -17,38 +18,37 @@ export function PostComponent(props: PostID){
 return (
     <View style = {styles.postContainer}>
         <View style = {styles.profileContainer}>
-            <Image source = {require('../assets/images/feedProfileImg.jpeg')} style = {styles.profileImage}/>
-        </View>
-        
-        <View style={styles.postInfo}>
-            <Text style = {[useColorScheme() === 'dark' ?  styles.usernameDark : styles.usernameLight]}>**Name**</Text>
-            <Text style = {[useColorScheme() === 'dark' ?  styles.dateDark : styles.dateLight]}>**Date Posted**</Text>
+            <Image source = {require('@/assets/images/profilepic.png')} style = {styles.profileImage}/>
+            <View style={styles.postInfo}>
+                <ThemedText type="defaultSemiBold">**Name**</ThemedText>
+                <ThemedText>**Date Posted**</ThemedText>
+            </View>
         </View>
         
         <View style = {styles.postContents}>
-            <ThemedText darkColor='#D3D3D3' lightColor='black'>**Post Text**</ThemedText>
-            <Image source = {require('../assets/images/post_example.jpeg')} style = {styles.postImage}/>
+            <Text style = {[useColorScheme() === 'dark' ?  styles.postTextDark : styles.postTextLight]}>**Post Text**</Text>
+            <Image source = {require('@/assets/images/post_example.jpeg')} style = {styles.postImage}/>
         </View>
 
         <View style = {styles.feedButtons}>
 
-            <Pressable onPress={() => alert('Liked!')} activeOpacity={0.5}>
-                <ThemedView style = {styles.likeButton}>
+            <Pressable onPress={() => alert('Liked!')}>
+                <View style = {styles.likeButton}>
                     <AntDesign name="like2" size={24} color={useColorScheme() === 'dark' ? "#D3D3D3" : "black"} />
-                </ThemedView>
+                </View>
             </Pressable>
 
-            <Pressable onPress={() => alert('Commented!')}activeOpacity={0.5}>
-                <ThemedView style = {styles.likeButton}>
+            <Pressable onPress={() => alert('Commented!')}>
+                <View style = {styles.likeButton}>
                     <AntDesign name="message1" size={24} color={useColorScheme() === 'dark' ? "#D3D3D3" : "black"} />
-                </ThemedView>
+                </View>
             </Pressable>
 
 
-            <Pressable onPress={() => alert('Shared!')}activeOpacity={0.5}>
-                <ThemedView style = {styles.likeButton}>
+            <Pressable onPress={() => alert('Shared!')}>
+                <View style = {styles.likeButton}>
                     <AntDesign name="doubleright" size={24} color={useColorScheme() === 'dark' ? "#D3D3D3" : "black"} />
-                </ThemedView>
+                </View>
             </Pressable>
         </View>
     </View>
@@ -94,18 +94,6 @@ const styles = StyleSheet.create({
         fontSize: 20,
         color: 'black',
     },
-    postImage: {
-        width: '100%',
-        height: 200,
-        resizeMode: 'cover',
-        borderRadius: 10,
-        marginTop: 10,
-    },
-    postInfo: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        marginTop: 10,
-    },
     dateDark: {
         fontSize: 16,
         color: '#D3D3D3',
@@ -117,24 +105,17 @@ const styles = StyleSheet.create({
     postContents: {
         
     },
-    profileContainer: {
-        flexDirection: 'row',
-        gap: 8,
-    },
     postTextLight: {
+        marginTop: 10,
         fontSize: 16,
         color: 'black',
     },
     postTextDark: {
+        marginTop: 20,
         fontSize: 16,
         color: '#D3D3D3',
     },
-    profileImage: {
-        width: 50,
-        height: 50,
-        borderRadius: 25,
-    },
-    headerImage: {
+    postImage: {
         width: '100%',
         height: 200,
         resizeMode: 'cover',
