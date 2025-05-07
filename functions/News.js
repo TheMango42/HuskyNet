08c9a1c2be400149273a86c3521268d2b75fbe58
AnakynSaylor <ajsaylor@mtu.edu>
2025-05-07 09:33:00 -0400

    Changed it to work for any rss feed

--- functions/News.js
+++ functions/News.js
@@ -1,19 +1,13 @@
 import { DOMParser } from 'xmldom';
 
-export async function readRss() {
-    const THELODE = 'https://mtulode.com/category/news/feed/';
+export async function readRss(rssFeedUrl) {
     const a = [];
-    const response = await fetch(THELODE);
+    const response = await fetch(rssFeedUrl);
     const str = await response.text();
     const data = new DOMParser().parseFromString(str, "text/xml");
     const items = data.getElementsByTagName("item");
     for(let i = 0; i < items.length; i++){
         a.push(items[i].textContent);
     }
-    for(let i = 0; i<a.length; i++){
-        console.log(a[i]);
-    }
     return a;
 }
-
-readRss();
