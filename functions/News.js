4b2f8035904c142541db1349e79d25f85bba9cef
AnakynSaylor <ajsaylor@mtu.edu>
2025-05-11 17:40:04 -0400

    expo sdk update 53

--- functions/News.js
+++ functions/News.js
@@ -1,13 +1,17 @@
-import { DOMParser } from 'xmldom';
+import { XMLParser, XMLBuilder } from 'fast-xml-parser';
 
 export async function readRss(rssFeedUrl) {
     const a = [];
     const response = await fetch(rssFeedUrl);
     const str = await response.text();
-    const data = new DOMParser().parseFromString(str, "text/xml");
-    const items = data.getElementsByTagName("item");
+    const data = new XMLParser().parse(str, "text/xml");
+    const builder = new XMLBuilder();
+    const items = builder.build(data);
     for(let i = 0; i < items.length; i++){
-        a.push(items[i].textContent);
+        a.push(items);
+        console.log(items);
     }
     return a;
 }
+const Lode = 'https://mtulode.com/category/news/feed/';
+readRss(Lode);
