579b490ba887f1dbd824654c4ee8326c3ef8eba1
AnakynSaylor <ajsaylor@mtu.edu>
2025-05-11 18:40:01 -0400

    Update News.js
    Changed rss reader to work with a different more secure xml parser and now it outputs an array with title, link, comments, date published, category, guid, and description for each item on the xml document tested with the lodes rss feed

--- functions/News.js
+++ functions/News.js
@@ -1,17 +1,18 @@
 import { XMLParser, XMLBuilder } from 'fast-xml-parser';
 
 export async function readRss(rssFeedUrl) {
-    const a = [];
     const response = await fetch(rssFeedUrl);
     const str = await response.text();
-    const data = new XMLParser().parse(str, "text/xml");
-    const builder = new XMLBuilder();
-    const items = builder.build(data);
-    for(let i = 0; i < items.length; i++){
-        a.push(items);
-        console.log(items);
+    const options = {
+        ignoreAttributes : false,
+        attributeNamePrefix: '@_'
+    };
+    const data = new XMLParser(options).parse(str);
+    const items = data.rss?.channel?.item ?? [];
+
+    if (!Array.isArray(items)) {
+        console.warn("No <item> nodes found");
+        return [];
     }
-    return a;
+    return items;
 }
-const Lode = 'https://mtulode.com/category/news/feed/';
-readRss(Lode);
